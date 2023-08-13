e452005b22787061e83c1f5d5719d8d1180c811f
HilmanShinichi <50318300+HilmanShinichi@users.noreply.github.com>
2023-08-13 21:16:07 +0700

    3PageLayout

---
+++ src/pages/product/[product].tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+
+
+const DetailProductPage = () => {
+  const {query} = useRouter()
+  return (
+    <div>
+        <h1>Detail Product</h1>
+        <p>Product: {query.product}</p>
+    </div>
+  )
+}
+
+export default DetailProductPage
